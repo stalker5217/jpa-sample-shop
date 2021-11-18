ce3d5ed00b5175f9c996e604176ac626772a6f9a
Song <stalker5217@gmail.com>
2021-11-19 01:02:39 +0900

    Entity 설계

---
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Embeddable
+@Getter
+public class Address {
+    private String city;
+    private String street;
+    private String zipcode;
+}
