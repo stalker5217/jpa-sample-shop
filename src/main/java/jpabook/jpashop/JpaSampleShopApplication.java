8dc83fc029e2173f7ac4bb888754194e4d25e9a8
Song <stalker5217@gmail.com>
2021-11-18 17:30:36 +0900

    first commit

---
+++ src/main/java/jpabook/jpashop/JpaSampleShopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSampleShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSampleShopApplication.class, args);
+    }
+
+}
